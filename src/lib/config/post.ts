ad329942bc4a15a831671d930ae19c070b416502
Hubert Chen <38517305+interstellar750@users.noreply.github.com>
2022-12-01 10:28:15 +0800

    feat: ✨ add Giscus

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,3 +1,18 @@
 import type { PostConfig } from '$lib/types/post'
 
-export const post: PostConfig = {}
+export const post: PostConfig = {
+	comment: {
+    	use: ['Giscus'],
+    	style: 'boxed',
+    	giscus: {
+      		repo: 'interstellar750/hexo_s',
+      		repoID: 'R_kgDOHTJG_w',
+      		category: 'General',
+      		categoryID: 'DIC_kwDOHTJG_84CS2Mz',
+      		reactionsEnabled: true,
+      		lang: 'zh-CN',
+	  		inputPosition: 'top',
+      		theme: 'preferred_color_scheme'
+    	}
+	}
+}
