88cde9e887fff5f8b47c0430e70945f8ff730376
Hubert Chen <01@trle5.xyz>
2022-12-20 21:17:59 +0800

    BREAKING CHANGE: ⬆ try sync upstream

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,7 @@
 import type { LayoutLoad } from './$types'
 export const prerender = true
-export const load: LayoutLoad = async ({ url, fetch }) => ({
+export const trailingSlash = 'always'
+export const load: LayoutLoad = async ({ url, fefetchtch }) => ({
   path: url.pathname,
   res: await fetch('/posts.json').then(res => res.json())
 })
