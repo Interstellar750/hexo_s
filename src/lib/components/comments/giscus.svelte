d7bbda825487ef8d5e8635faf1697a581264147f
Hubert Chen <01@trle5.xyz>
2022-12-24 18:06:02 +0800

    config(giscus): 📝 change mapping

--- src/lib/components/comments/giscus.svelte
+++ src/lib/components/comments/giscus.svelte
@@ -12,7 +12,7 @@
       'data-repo-id': config.repoID,
       'data-category': config.category ?? '',
       'data-category-id': config.categoryID,
-      'data-mapping': 'og:title',
+      'data-mapping': 'pathname',
       'data-reactions-enabled': config.reactionsEnabled === false ? '0' : '1',
       'data-input-position': config.inputPosition ?? 'bottom',
       'data-theme': config.theme ?? 'preferred_color_scheme',
