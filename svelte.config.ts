44568458efe26f1ab68fe607248d7e83c206dca5
Hubert Chen <01@trle5.xyz>
2023-01-23 12:09:10 +0800

    deps: ⬇ downgrade sveltekit & vite

--- svelte.config.ts
+++ svelte.config.ts
@@ -7,13 +7,13 @@ import adapterStatic from '@sveltejs/adapter-static'
 // svelte preprocessor
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import preprocess from 'svelte-preprocess'
 
 const defineConfig = (config: Config) => config
 
 export default defineConfig({
   extensions: ['.svelte', ...(mdsvexConfig.extensions as string[])],
-  preprocess: [mdsvex(mdsvexConfig), vitePreprocess()],
+  preprocess: [mdsvex(mdsvexConfig), preprocess()],
   kit: {
     adapter: Object.keys(process.env).some(key => ['VERCEL', 'CF_PAGES', 'NETLIFY'].includes(key))
       ? adapterAuto()
